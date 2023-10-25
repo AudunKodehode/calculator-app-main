9a2ca49e6832d5b7632dbf2b1ca86524bc3bf36c
AudunKodehode <audunkodehode@gmail.com>
2023-10-25 17:00:00 +0200

    *

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <>
     <Calculator/>
+    <div className="fem">
+    Challenge by <a href="https://www.frontendmentor.io/solutions/calculatorapp-FBFB67V_0">Frontend Mentor.</a> Coded by <a href="https://github.com/AudunKodehode">AudunKodehode</a>. 
+
+    </div>
     </>
   )
 }
